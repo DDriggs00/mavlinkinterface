528cb25cbbf64dd205b5e42b23678c347aaafb72
= <=>
2019-06-04 15:15:56 -0700

    Fixed __init__ files

--- mavlinkinterface/__init__.py
+++ mavlinkinterface/__init__.py
@@ -1,3 +1,9 @@
-from mavlinkinterface.main import mavlinkInterface
-from mavlinkinterface.main import MLIQueueModes
+
+# Import main function
+# from mavlinkinterface.main import mavlinkInterface
+from .main import *
+# Import enums
+# from mavlinkinterface.main import MLIQueueModes
+# from mavlinkinterface.main import MLFlightModes
+
 __all__ = ["mavlinkInterface", "MLIQueueModes"]
--- mavlinkinterface/commands/active/__init__.py
+++ mavlinkinterface/commands/active/__init__.py
@@ -1,5 +1,7 @@
 # Import all functions
 from mavlinkinterface.commands.active.lights import setLights
+from mavlinkinterface.commands.active.flightModes import setFlightMode
+from mavlinkinterface.commands.active.flightModes import MLFlightModes
 
 # For importing all at once
-__all__ = ["setLights"]
+__all__ = ["setLights", "setFlightMode", "MLFlightModes"]
