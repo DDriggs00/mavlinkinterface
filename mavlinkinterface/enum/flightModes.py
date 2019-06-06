7f50c9be8988c93054ac7b4827693ea6705f5e2b
= <=>
2019-06-06 15:59:18 -0700

    Corrected flight mode names

--- mavlinkinterface/enum/flightModes.py
+++ mavlinkinterface/enum/flightModes.py
@@ -1,28 +1,28 @@
 from enum import Enum
 
 class flightModes(Enum):
-    manual = 0
+    manual = 'MANUAL'
     # Manual mode passes the pilot inputs directly to the motors, with no stabilization.
     # ArduSub always boots in Manual mode.
-    stabilize = 1
+    stabilize = 'STABILIZE'
     # Stabilize mode is like Manual mode, with heading and attitude stabilization.
-    depth_hold = 2
+    depth_hold = 'ALT_HOLD'
     # Depth Hold is like Stabilize mode with the addition of depth stabilization
     # when the pilot throttle input is zero.
     # A depth sensor is required to use depth hold mode.
-    position_hold = 3  # REQUIRES GPS
+    position_hold = 'POSHOLD'  # REQUIRES GPS
     # Position Hold mode will stabilize the vehicle's absolute position, attitude,
     # and heading when the pilot control inputs are neutral.
     # The vehicle can be maneuvered and repositioned by the pilot.
-    auto = 4  # REQUIRES GPS
+    auto = 'AUTO'  # REQUIRES GPS
     # Auto mode executes the mission stored on the autopilot autonomously.
     # Pilot control inputs are ignored in most cases.
     # The vehicle may be disarmed, or the mode can be changed to abort the mission.
-    circle = 5  # REQUIRES GPS
+    circle = 'CIRCLE'  # REQUIRES GPS
     # Circle mode navigates in circles with the front of the vehicle facing the center point.
-    guided = 6  # REQUIRES GPS
+    guided = 'GUIDED'  # REQUIRES GPS
     # Guided mode allows the vehicle's target position to be set dynamically
     # by a ground control station or companion computer.
     # This allows 'Click to Navigate Here' interactions with a map.
-    acro = 7  # secret menu
+    acro = 'ACRO'  # secret menu
     # Acro (Acrobatic) mode performs angular rate stabilization.
