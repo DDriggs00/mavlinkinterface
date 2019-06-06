b0be6ffc92890acbdb6e05bbecba3d0c78617510
= <=>
2019-06-06 15:58:28 -0700

    Minor additions to examples

--- examples/example1.py
+++ examples/example1.py
@@ -4,8 +4,14 @@ import mavlinkinterface
 # Create interface object
 MLI = mavlinkinterface.mavlinkInterface()
 
+
 MLI.move("forward", 5, 50)
+
+# Strafing square
 MLI.move("forward", 5, 50)
 MLI.move("left", 5, 100)
 MLI.move("back", 5, 50)
 MLI.move("right", 5, 100)
+
+MLI.dive(5, -100)
+MLI.dive(5, 100)
