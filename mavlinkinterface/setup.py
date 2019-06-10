3b7bd52dc37d5f475bf50b3a4af0ec1bdf3c0edd
= <=>
2019-06-10 15:52:10 -0700

    Added setup.py

---
+++ mavlinkinterface/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mavlinkinterface',
+      version='1.0',
+      description='Interface with pymavlink',
+      author='Devin Driggs',
+      author_email='drig3819@vandals.uidaho.edu',
+      url='N/A',
+      packages=['mavlinkinterface'],
+      )
