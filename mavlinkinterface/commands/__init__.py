eb0bff49f9e483e21ef8ada40a034e85d7984ba6
= <=>
2019-06-12 09:00:22 -0700

    bugfix

--- mavlinkinterface/commands/__init__.py
+++ mavlinkinterface/commands/__init__.py
@@ -1,4 +1,5 @@
-import mavlinkinterface.commands.active as active
-import mavlinkinterface.commands.passive as passive
-import mavlinkinterface.commands.configuration as configuration
+from . import active
+from . import passive
+from . import configuration
+
 __all__ = ['active', 'passive', 'configuration']
