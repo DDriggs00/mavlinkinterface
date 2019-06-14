3dbae2bb0238eab4727f31b80a752f3965aa2979
= <=>
2019-06-14 15:52:33 -0700

    Improved documentation

--- mavlinkinterface/main.py
+++ mavlinkinterface/main.py
@@ -1,44 +1,50 @@
 # Regular Imports
 from pymavlink import mavutil           # For pretty much everything
 from threading import Thread            # For pretty much everything
-from threading import Semaphore         # For keeping only one thread running
+from threading import Semaphore         # To prevent multiple movement commands at once
 # from queue import Queue                 # For queuing mode
-from datetime import datetime           # For naming log file
 import logging                          # For logging
+from datetime import datetime           # For naming log file
+# import platform                         # For choosing log location
 from configparser import ConfigParser   # For configuration details
 
 # Local Imports
-import mavlinkinterface.commands as commands
-from mavlinkinterface.enum.queueModes import queueModes
-from mavlinkinterface.enum.flightModes import flightModes
+import mavlinkinterface.commands as commands                # For calling commands
+from mavlinkinterface.rthread import RThread                # For functions that have return values
+from mavlinkinterface.datarefresher import dataRefresher    # For keeping the message socket clean
+from mavlinkinterface.enum.queueModes import queueModes     # For use in async mode
 
 class mavlinkInterface(object):
     '''
-    This is the main interface to Mavlink
+    This is the main interface to Mavlink. All calls will be made through this object.
     '''
+    # Internal Commands
     def __init__(self, queueMode=queueModes.override, asynchronous=False):
         '''
         Creates a new mavlinkInterface Object
-        @param queueMode See docs/configuration/setDefaultQueueMode for details.\n
-        @param asynchronous When false or not given, movement commands will return once the movement is done.  When true, movement commands will return immediately and execute in the background.
+        :param queueMode: See docs/configuration/setDefaultQueueMode for details.\n
+        :param asynchronous: When false or not given, movement commands will return once the movement is done.  When true, movement commands will return immediately and execute in the background.
         '''
         # Set up Logging
         logFileName = ('log_' + str(datetime.now()) + '.log').replace(':', '.')
         logging.basicConfig(filename=logFileName,
                             format='%(asctime)s - %(levelname)s - %(message)s',
                             level=logging.DEBUG)
+
         # Import config values
         logging.debug("importing configuration file")
         self.config = ConfigParser()
         self.config.read("mavlinkinterface/config.cfg")
+
         # Set class variables
         logging.debug("Setting class variables")
         self.queueMode = queueMode
         self.asynchronous = asynchronous
         self.lightBrightness = 0
-        self.flightMode = flightModes.manual
+
         # Create Semaphore
         self.sem = Semaphore(1)
+
         # Set up Mavlink
         logging.info("Initializing MavLink Connection")
         connectionString = 'udp:' + self.config['mavlink']['connection_ip'] + ':' + self.config['mavlink']['connection_port']
@@ -47,13 +53,24 @@ class mavlinkInterface(object):
         logging.info("Successfully connected to target.")
         print("Successfully connected to target.")
 
-    def __del__(self):
-        self.disarm()
-        pass
+        # start dataRefresher
+        self.refresher = dataRefresher(self.mavlinkConnection)
+        logging.info("Started dataRefresher process")
 
-    def __getSemaphore(self, override):
+    def __del__(self):
+        '''Clean up while exiting
+        Note: No logging may happen here due to a bug when using exit() that I can't figure out
+        '''
+        # Stop dataRefresher process
+        self.refresher.kill()
+        # Disarm
+        self.__getSemaphore(override=True)
+        commands.active.disarm(self.mavlinkConnection, self.sem)
+
+    # Private functions
+    def __getSemaphore(self, override):     # contains TODO
         '''Attempts to acquire the movement semaphore based on queuemode. Returns true if semaphore was acquired, false otherwise.'''
-        if not self.sem.acquire(blocking=False):  # Semaphore could not be acquired, proceeding by mode
+        if not self.sem.acquire(blocking=False):    # Semaphore could not be acquired, proceeding by mode
             if self.queueMode == queueModes.override or override:
                 print("Override active, Killing existing task")
                 logging.info("Override active, Killing existing task")
@@ -66,14 +83,15 @@ class mavlinkInterface(object):
             elif self.queueMode == queueModes.ignore:
                 print("Using Ignore mode, this command will now be discarded")
                 logging.info("Using Ignore mode, command ignored")
-                return False    # The command needs not be executed
+                return False    # The command should not be executed
             elif self.queueMode == queueModes.queue:
                 print("Using queue Mode, Adding item to queue")
                 logging.info("Using queue Mode, Adding item to queue")
-                print("This mode does nothing atm. The command will be ignored")  # TODO
+                print("This mode does nothing currently. The command will be ignored")  # TODO
                 return False    # The command needs not be executed
-        return True
+        return True     # If the semaphore was obtained on the first try
 
+    # General commands
     def help(self):     # TODO
         print("Available functions:")
         print("move(direction, ):")
@@ -81,9 +99,9 @@ class mavlinkInterface(object):
         print("setLights(brightness)")
         print("setFlightMode(mode)")
 
-    def stopAll(self):  # TODO finish
+    def stopAll(self):  # contains TODO
         if self.queueMode == queueModes.queue:
-            # Clear queue
+            # TODO Clear queue
             pass
         self.stopCurrentTask()
 
@@ -91,35 +109,44 @@ class mavlinkInterface(object):
         # Kills the currently running task and stops the submarine
         pass
 
-    def setLightsMax(self, override=False):
+    # Active commands
+    def arm(self, override=False):
+        '''Enables the thrusters'''
         if not self.__getSemaphore(override):
             return
 
-        # self.lightBrightness = brightness
-        t = Thread(target=commands.active.lightsMax, args=(self.mavlinkConnection, self.sem,))
-        t.start()
+        logging.info("Arming")
+        self.t = Thread(target=commands.active.arm, args=(self.mavlinkConnection, self.sem,))
+        self.t.start()
         if(not self.asynchronous):
-            t.join()
+            self.t.join()
 
-    def setLightsOff(self, override=False):
+    def disarm(self, override=False):
+        '''Disables the thrusters'''
         if not self.__getSemaphore(override):
             return
 
-        # self.lightBrightness = brightness
-        t = Thread(target=commands.active.lightsoff, args=(self.mavlinkConnection, self.sem,))
-        t.start()
+        logging.info("Disarming")
+        self.t = Thread(target=commands.active.disarm, args=(self.mavlinkConnection, self.sem,))
+        self.t.start()
         if(not self.asynchronous):
-            t.join()
+            self.t.join()
 
     def dive(self, time, throttle, override=False):
+        '''
+        Thrust vertically for a specified amount of time
+
+        :param time: how long to thrust in seconds
+        :param throttle: percent throttle to use, -100 = full down, 100 = full up
+        '''
         if not self.__getSemaphore(override):
             return
 
         logging.info("Diving at " + str(throttle) + "% throttle for " + str(time) + " seconds")
-        t = Thread(target=commands.active.dive, args=(self.mavlinkConnection, self.sem, time, throttle,))
-        t.start()
+        self.t = Thread(target=commands.active.dive, args=(self.mavlinkConnection, self.sem, time, throttle,))
+        self.t.start()
         if(not self.asynchronous):
-            t.join()
+            self.t.join()
 
     def yaw(self, angle, override=False):
         '''Rotates the drone around the Z-Axis
@@ -130,12 +157,129 @@ class mavlinkInterface(object):
             return
 
         logging.info("Yawing " + str(angle) + " degrees")
-        t = Thread(target=commands.active.yaw, args=(self.mavlinkConnection, self.sem, angle,))
-        t.start()
+        self.t = Thread(target=commands.active.yaw, args=(self.mavlinkConnection, self.sem, angle,))
+        self.t.start()
         if(not self.asynchronous):
-            t.join()
+            self.t.join()
 
-    def yawBeta(self, angle, rate=20, direction=1, relative=1, override=False):
+    def move(self, direction, time, throttle=100, absolute=False, override=False):
+        '''
+        Move horizontally in any direction
+
+        Parameter Direction: the angle (in degrees) to move toward
+        Parameter time: the time (in seconds) to power the thrusters
+        Parameter throttle: the percentage of thruster power to use
+        Parameter Absolute: When true, an angle of 0 degrees is magnetic north
+        '''
+        if not self.__getSemaphore(override):
+            return
+
+        logging.info("Moving in direction: " + str(direction) + " at " + str(throttle) + "% throttle for " + str(time) + "seconds")
+        self.t = Thread(target=commands.active.move, args=(self.mavlinkConnection, self.sem, direction, time, throttle,))
+        self.t.start()
+        if(not self.asynchronous):
+            self.t.join()
+
+    def move3d(self, throttleX, throttleY, throttleZ, time, override=False):
+        '''
+        Move in any direction
+
+        Parameter Throttle X: Percent power to use when thrusting in the X direction
+        Parameter Throttle Y: Percent power to use when thrusting in the Y direction
+        Parameter Throttle Z: Percent power to use when thrusting in the Z direction
+        Parameter Time: The time (in seconds) to power the thrusters
+        '''
+        if not self.__getSemaphore(override):
+            return
+
+        logging.info("Moving in direction x=" + str(throttleX) + " y=" + str(throttleY) + " z=" + str(throttleZ) + " for " + str(time) + "seconds")
+        self.t = Thread(target=commands.active.move3d, args=(self.mavlinkConnection, self.sem, throttleX, throttleY, throttleZ, time,))
+        self.t.start()
+        if(not self.asynchronous):
+            self.t.join()
+
+    def setFlightMode(self, mode, override=False):
+        '''
+        Sets the flight mode of the drone.
+        Valid modes are listed in docs/active/setFlightMode.md
+
+        Parameter Mode: The mode to use
+        '''
+        if not self.__getSemaphore(override):
+            return
+
+        logging.info("Setting flight mode to " + str(mode))
+        self.t = Thread(target=commands.active.setFlightMode, args=(self.mavlinkConnection, self.sem, mode,))
+        self.t.start()
+        if(not self.asynchronous):
+            self.t.join()
+
+    # def setLights(self, brightness, override=False):     # Broken
+    #     '''Sets the lights to the specified brightness(rounded to the nearest brightness level)
+
+    #     Parameter brightness: The percentage brightness to use for the lights
+    #     '''
+    #     if not self.__getSemaphore(override):
+    #         return
+
+    #     # self.lightBrightness = brightness
+    #     self.t = Thread(target=commands.active.lightsMax, args=(self.mavlinkConnection, self.sem,))
+    #     self.t.start()
+    #     if(not self.asynchronous):
+    #         self.t.join()
+
+    # Sensor reading commands
+    def getBatteryData(self):
+        '''Returns a JSON containing battery Data'''
+        logging.info("Fetching battery Data JSON")
+        self.t = RThread(target=commands.passive.getBatteryData, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    def getAccelerometerData(self):
+        '''Returns a JSON containing Accelerometer Data'''
+        logging.info("Fetching Accelerometer Data JSON")
+        self.t = RThread(target=commands.passive.getAccelerometerData, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    def getGyroscopeData(self):
+        '''Returns a JSON containing Gyroscope Data'''
+        logging.info("Fetching Gyroscope Data JSON")
+        self.t = RThread(target=commands.passive.getGyroscopeData, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    def getMagnetometerData(self):
+        '''Returns a JSON containing Magnetometer Data'''
+        logging.info("Fetching Magnetometer Data JSON")
+        self.t = RThread(target=commands.passive.getMagnetometerData, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    def getIMUData(self):
+        '''Returns a JSON containing IMU Data'''
+        logging.info("Fetching IMU Data JSON")
+        self.t = RThread(target=commands.passive.getIMUData, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    def getPressureExternal(self):
+        '''Returns the external pressure as a float'''
+        logging.info("Fetching External Pressure")
+        self.t = RThread(target=commands.passive.getPressureExternal, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    def getDepth(self):
+        '''Returns the depth of the drone as a float'''
+        logging.info("Fetching Depth")
+        self.t = RThread(target=commands.passive.getDepth, args=(self.mavlinkConnection,))
+        self.t.start()
+        return self.t.join()
+
+    # Beta Commands
+    def yawBeta(self, angle, rate=20, direction=1, relative=1, override=False):     # Broken
         '''Rotates the drone around the Z-Axis
 
         angle: distance to rotate in degrees
@@ -150,93 +294,17 @@ class mavlinkInterface(object):
             return
 
         logging.info("Yawing " + ("clockwise by " if (direction == 1) else "Counterclockwise by ") + str(angle) + " degrees at " + str(rate) + " deg/s in " + ("relative" if (relative == 1) else "Absolute") + " mode.")
-        t = Thread(target=commands.active.yawBeta, args=(self.mavlinkConnection, self.sem, angle, rate, direction, relative,))
-        t.start()
+        self.t = Thread(target=commands.active.yawBeta, args=(self.mavlinkConnection, self.sem, angle, rate, direction, relative,))
+        self.t.start()
         if(not self.asynchronous):
-            t.join()
+            self.t.join()
 
     def changeAltitude(self, rate, altitude, override=False):
         if not self.__getSemaphore(override):
             return
 
         logging.info("Moving to altitude " + str(altitude) + " at " + str(rate) + " m/s.")
-        t = Thread(target=commands.active.changeAltitude, args=(self.mavlinkConnection, self.sem, rate, altitude,))
-        t.start()
+        self.t = Thread(target=commands.active.changeAltitude, args=(self.mavlinkConnection, self.sem, rate, altitude,))
+        self.t.start()
         if(not self.asynchronous):
-            t.join()
-
-    def move(self, direction, time, throttle=100, override=False):
-        if not self.__getSemaphore(override):
-            return
-
-        logging.info("Moving in direction: " + str(direction) + " at " + str(throttle) + "% throttle for " + str(time) + "seconds")
-        t = Thread(target=commands.active.move, args=(self.mavlinkConnection, self.sem, direction, time, throttle,))
-        t.start()
-        if(not self.asynchronous):
-            t.join()
-
-    def move3d(self, throttleX, throttleY, throttleZ, time, override=False):
-        if not self.__getSemaphore(override):
-            return
-
-        logging.info("Moving in direction x=" + str(throttleX) + " y=" + str(throttleY) + " z=" + str(throttleZ) + " for " + str(time) + "seconds")
-        t = Thread(target=commands.active.move3d, args=(self.mavlinkConnection, self.sem, throttleX, throttleY, throttleZ, time,))
-        t.start()
-        if(not self.asynchronous):
-            t.join()
-
-    def setFlightMode(self, mode, override=False):
-        if not self.__getSemaphore(override):
-            return
-
-        logging.info("Setting flight mode to " + str(mode))
-        self.flightMode = mode
-        t = Thread(target=commands.active.setFlightMode, args=(self.mavlinkConnection, self.sem, mode,))
-        t.start()
-        if(not self.asynchronous):
-            t.join()
-
-    def getFlightMode(self):
-        return self.flightMode
-
-    def arm(self, override=False):
-        if not self.__getSemaphore(override):
-            return
-
-        logging.info("Arming")
-        t = Thread(target=commands.active.arm, args=(self.mavlinkConnection, self.sem,))
-        t.start()
-        if(not self.asynchronous):
-            t.join()
-
-    def disarm(self, override=False):
-        if not self.__getSemaphore(override):
-            return
-
-        logging.info("Disarming")
-        t = Thread(target=commands.active.disarm, args=(self.mavlinkConnection, self.sem,))
-        t.start()
-        if(not self.asynchronous):
-            t.join()
-
-
-# class queueManager(Queue):
-#     def __init__(self, maxsize):
-#         return super().__init__(maxsize)
-
-class RThread(Thread):    # https://stackoverflow.com/questions/6893968/how-to-get-the-return-value-from-a-thread-in-python
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-
-    def run(self):
-        try:
-            if self._target is not None:
-                self._return = self._target(*self._args, **self._kwargs)
-        finally:
-            del self._target, self._args, self._kwargs
-
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
+            self.t.join()
