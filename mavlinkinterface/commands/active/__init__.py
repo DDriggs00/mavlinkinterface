792d6a2c2e32abfa2366273bd299a163e58e0704
= <=>
2019-06-14 09:16:23 -0700

    Updated which files to include

--- mavlinkinterface/commands/active/__init__.py
+++ mavlinkinterface/commands/active/__init__.py
@@ -1,16 +1,19 @@
 # Import all functions
 from mavlinkinterface.commands.active.lights import setLights
 from mavlinkinterface.commands.active.flightModes import setFlightMode
-from mavlinkinterface.commands.active.movement import move, move3d, dive, yaw, yaw2
+from mavlinkinterface.commands.active.arm_disarm import arm, disarm
+from mavlinkinterface.commands.active.movement import move, move3d, dive, yaw, yawBeta
 from mavlinkinterface.commands.active.beta_commands import changeAltitude, lightsMax, lightsoff
 
 __all__ = [
+    "arm",
+    "disarm",
     "setLights",
     "setFlightMode",
     "move",
     "move3d",
     "yaw",
-    "yaw2",
+    "yawBeta",
     "dive",
     "changeAltitude",
     "lightsMax",
