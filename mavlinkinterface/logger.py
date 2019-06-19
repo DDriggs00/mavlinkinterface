4ca5a8cb76ec78c9a88c3d233de67451e277411d
= <=>
2019-06-19 15:26:42 -0700

    Updated Logger

--- mavlinkinterface/logger.py
+++ mavlinkinterface/logger.py
@@ -1,20 +1,25 @@
 import logging
 from datetime import date
 
-def getLogger(name, fileName=None):
-    logFormat = '%(asctime)s, %(name)8s, %(levelname)5s, %(message)s'
-    consoleFormat = '%(message)s'
-    if not fileName:
-        logFileName = ('log_' + str(date.today()) + '.log')
+def getLogger(name, fileName=None, doPrint=False, basic=False):
+    if basic:
+        logFormat = '%(asctime)s, %(message)s'
     else:
+        logFormat = '%(asctime)s, %(name)8s, %(levelname)5s, %(message)s'
+
+    if fileName:
         logFileName = fileName
+    else:
+        logFileName = ('log_' + str(date.today()) + '.log')
+
     logging.basicConfig(level=logging.DEBUG,
                         format=logFormat,
                         filename=logFileName,
                         filemode='w')
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    console.setFormatter(logging.Formatter(consoleFormat))
-    logging.getLogger(name).addHandler(console)
-    logging.getLogger(name).debug("Logger Successfully Initiated")
+    if doPrint:
+        consoleFormat = '%(message)s'
+        console = logging.StreamHandler()
+        console.setLevel(logging.INFO)
+        console.setFormatter(logging.Formatter(consoleFormat))
+        logging.getLogger(name).addHandler(console)
     return logging.getLogger(name)
