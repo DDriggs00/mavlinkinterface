76a3ff4a218ec24dace65f4190807d54c56489b6
DDriggs00 <DDriggs00@gmail.com>
2019-06-28 11:45:23 -0700

    Modified test scripts to run with py3 when launched from shell

--- examples/StrafeSquare.py
+++ examples/StrafeSquare.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import mavlinkinterface
 MLI = mavlinkinterface.mavlinkInterface()
 MLI.arm()
--- examples/execModeTests.py
+++ examples/execModeTests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import mavlinkinterface     # Needed to use the library
 
@@ -7,8 +8,13 @@ MLI = mavlinkinterface.mavlinkInterface(execMode="queue")
 
 MLI.arm()   # Enable the propellors
 
+# Add some commands to the queue
 MLI.move(90, 3)     # Strafe right for 3 sec
-MLI.move(270, 3)    # Move left for 3 sec
+MLI.move(270, 3)    # Strafe left  for 3 sec
+MLI.move(90, 3)     # Strafe right for 3 sec
+MLI.move(270, 3)    # Strafe left  for 3 sec
+MLI.move(90, 3)     # Strafe right for 3 sec
+MLI.move(270, 3)    # Strafe left  for 3 sec
 
 
 MLI.move(0, 3, execMode="override")      # Move forward for 3 sec
