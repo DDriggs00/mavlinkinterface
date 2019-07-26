d52bd73ed35d74084856c3b0b1b5bb7f9e3579d3
DDriggs00 <DDriggs00@gmail.com>
2019-07-26 13:26:52 -0700

    fixed lights

--- mavlinkinterface/commands/active/lights.py
+++ mavlinkinterface/commands/active/lights.py
@@ -8,14 +8,17 @@ class lights(object):
 
     steps = 8   # number of brightness levels the light has
 
-    def __init__(self, ml):
+    def __init__(self, mli):
         for i in range(0, self.steps):
-            self.__down(ml)
-            sleep(0.05)
+            self.__down(mli)
+            sleep(0.04)
         self.level = 0
         self.log = getLogger("Lights")
 
-    def set(self, ml, sem, brightness):
+    def set(self, mli, sem, brightness):
+        if brightness > 100 or brightness < 0:
+            self.log.warn('Brightness must be from 0 to 100')
+
         self.log.info("Setting Lights to " + str(brightness) + "% brightness")
         try:
             desiredLevel = round(0.09 * brightness)
@@ -23,51 +26,38 @@ class lights(object):
             self.log.trace("desired lighting level = " + str(brightness))
 
             while desiredLevel > self.level:
-                self.__up(ml)
+                self.__up(mli)
                 self.level += 1
                 sleep(0.05)
 
             while desiredLevel < self.level:
-                self.__down(ml)
+                self.__down(mli)
                 self.level -= 1
                 sleep(0.05)
         finally:
             sem.release()
 
-    def __up(self, ml):
-        buttons = 1 << 14
-        ml.mav.manual_control_send(
-            ml.target_system,
-            0,          # x [ Range: -1000-1000; backward=-1000, forward=1000, 0 = No X-Axis thrust ]
-            0,          # y [ Range: -1000-1000; Left=-1000, Right=1000, 0 = No Y-Axis thrust ]
-            500,        # z [ Range: 0-1000; 0=down, 1000=up; 500 = no vertical thrust ]
-            0,          # r [ Yaw, with counter-clockwise being negative. ]
-            buttons)    # b [ A bitfield corresponding to the joystick buttons' current state, 1 == pressed]
-        sleep(0.05)
-        # Reset button positions to zero
-        ml.mav.manual_control_send(
-            ml.target_system,
-            0,    # x [ Range: -1000-1000; backward=-1000, forward=1000, 0 = No X-Axis thrust ]
-            0,    # y [ Range: -1000-1000; Left=-1000, Right=1000, 0 = No Y-Axis thrust ]
-            500,  # z [ Range: 0-1000; 0=down, 1000=up; 500 = no vertical thrust ]
-            0,    # r [ Yaw, with counter-clockwise being negative. ]
-            0)    # b [ A bitfield corresponding to the joystick buttons' current state, 1 == pressed]
+    def __up(self, mli):
 
-    def __down(self, ml):
-        buttons = 1 << 13
-        ml.mav.manual_control_send(
-            ml.target_system,
-            0,          # x [ Range: -1000-1000; backward=-1000, forward=1000, 0 = No X-Axis thrust ]
-            0,          # y [ Range: -1000-1000; Left=-1000, Right=1000, 0 = No Y-Axis thrust ]
-            500,        # z [ Range: 0-1000; 0=down, 1000=up; 500 = no vertical thrust ]
-            0,          # r [ Yaw, with counter-clockwise being negative. ]
-            buttons)    # b [ A bitfield corresponding to the joystick buttons' current state, 1 == pressed]
+        # Because the 
+        mli.mavlinkConnection.mav.manual_control_send(
+            mli.mavlinkConnection.target_system,
+            mli.manualControlParams['x'],  # X-Axis thrust
+            mli.manualControlParams['y'],  # Y-Axis thrust
+            mli.manualControlParams['z'],  # Z-Axis thrust
+            mli.manualControlParams['r'],  # R-Axis thrust
+            (mli.manualControlParams['b'] | 1 << 14)
+        )
         sleep(0.05)
-        # Reset button positions to zero
-        ml.mav.manual_control_send(
-            ml.target_system,
-            0,    # x [ Range: -1000-1000; backward=-1000, forward=1000, 0 = No X-Axis thrust ]
-            0,    # y [ Range: -1000-1000; Left=-1000, Right=1000, 0 = No Y-Axis thrust ]
-            500,  # z [ Range: 0-1000; 0=down, 1000=up; 500 = no vertical thrust ]
-            0,    # r [ Yaw, with counter-clockwise being negative. ]
-            0)    # b [ A bitfield corresponding to the joystick buttons' current state, 1 == pressed]
+
+    def __down(self, mli):
+
+        mli.mavlinkConnection.mav.manual_control_send(
+            mli.mavlinkConnection.target_system,
+            mli.manualControlParams['x'],  # X-Axis thrust
+            mli.manualControlParams['y'],  # Y-Axis thrust
+            mli.manualControlParams['z'],  # Z-Axis thrust
+            mli.manualControlParams['r'],  # R-Axis thrust
+            (mli.manualControlParams['b'] | 1 << 13)
+        )
+        sleep(0.05)
